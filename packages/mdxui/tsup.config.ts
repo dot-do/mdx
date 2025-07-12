0e0e551bbd772a46a2599eb2aaba351ffd0dcdde
Chris Risner <chris@driv.ly>
2025-07-11 20:01:43 -0500

    build: update tsup for external framer-motion to just motion now

--- packages/mdxui/tsup.config.ts
+++ packages/mdxui/tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
   dts: true,
-  external: ['react', 'react-dom', 'framer-motion', '@tremor/react', 'zod'],
+  external: ['react', 'react-dom', 'motion', '@tremor/react', 'zod'],
   banner: {
     js: '"use client";',
   },
